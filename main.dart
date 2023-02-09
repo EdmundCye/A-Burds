a767aedc5eb1802c7440f867504c1b18b80ab167
EdmundCye <111274518+EdmundCye@users.noreply.github.com>
2023-02-09 14:38:54 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:app/auth_controller.dart';
+import 'package:app/login_page.dart';
+import 'package:app/signup_collector_page.dart';
+import 'package:app/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'signup_collector_page.dart';
+import 'signup_page.dart';
+import 'welcome_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginPage());
+  }
+}
