204a521824cce3ec3203af03b442d795b2862e91
EdmundCye <111274518+EdmundCye@users.noreply.github.com>
2023-02-09 14:50:04 +0800

    lib

---
+++ signup_page.dart
@@ -0,0 +1,242 @@
+// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
+// ignore_for_file: avoid_unnecessary_containers
+import 'auth_controller.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter/services.dart';
+import 'package:otp/otp.dart';
+import 'package:sms_autofill/sms_autofill.dart';
+
+import 'otp_page.dart';
+
+class SignUpPage extends StatelessWidget {
+  @override //For OTP
+  Widget build(BuildContext context) {
+    TextEditingController mobileNumber = TextEditingController();
+
+    void submit() async {
+      if (mobileNumber.text == "") return;
+
+      var appSignatureID = await SmsAutoFill().getAppSignature;
+      Map sendOtpData = {
+        "mobile_number": mobileNumber.text,
+        "app_signature_id": appSignatureID
+      };
+      print(sendOtpData);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const VerifyOTPScreen()),
+      );
+    }
+
+    var emailController = TextEditingController();
+    var passwordController = TextEditingController();
+
+    List images = ["g.png", "t.png", "f.png"];
+
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Container(
+                width: w,
+                height: h * 0.3,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("img/signup.png"),
+                        fit: BoxFit.cover)),
+                child: Column(children: [
+                  SizedBox(
+                    height: h * 0.1,
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Colors.white70,
+                    radius: 60,
+                    backgroundImage: AssetImage("img/profile1.png"),
+                  )
+                ])),
+            Container(
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                width: w,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 10,
+                              spreadRadius: 7,
+                              offset: Offset(1, 1),
+                              color: Colors.grey.withOpacity(0.2),
+                            )
+                          ]),
+                      child: TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                              hintText: "Email",
+                              prefixIcon:
+                                  Icon(Icons.email, color: Colors.green[800]),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(
+                                      color: Colors.white, width: 1.0)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(
+                                      color: Colors.white, width: 1.0)),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 10,
+                              spreadRadius: 7,
+                              offset: Offset(1, 1),
+                              color: Colors.grey.withOpacity(0.2),
+                            )
+                          ]),
+                      child: TextField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              hintText: "Password",
+                              prefixIcon: Icon(Icons.password_outlined,
+                                  color: Colors.green[800]),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(
+                                      color: Colors.white, width: 1.0)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(
+                                      color: Colors.white, width: 1.0)),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 10,
+                              spreadRadius: 7,
+                              offset: Offset(1, 1),
+                              color: Colors.grey.withOpacity(0.2),
+                            )
+                          ]),
+                      child: TextField(
+                          controller: mobileNumber,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          decoration: InputDecoration(
+                              hintText: "",
+                              prefixIcon:
+                                  Icon(Icons.phone, color: Colors.green[800]),
+                              prefixText: "+60 ",
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(
+                                      color: Colors.white, width: 1.0)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(
+                                      color: Colors.white, width: 1.0)),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ))),
+                    ),
+                  ],
+                )),
+            SizedBox(
+              height: 110,
+            ),
+            GestureDetector(
+              onTap: () {
+                AuthController.instance.register(emailController.text.trim(),
+                    passwordController.text.trim());
+                submit();
+              },
+              child: Container(
+                width: w * 0.5,
+                height: h * 0.08,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    image: DecorationImage(
+                        image: AssetImage("img/loginbtn.png"),
+                        fit: BoxFit.cover)),
+                child: Center(
+                  child: Text(
+                    "Sign up",
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            RichText(
+                text: TextSpan(
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () => Get.back(),
+                    text: "Have an account?",
+                    style: TextStyle(
+                      fontSize: 21,
+                      color: Colors.grey[500],
+                    ))),
+            SizedBox(
+              height: w * 0.13,
+            ),
+            RichText(
+                text: TextSpan(
+              text: "Sign up using following method",
+              style: TextStyle(color: Colors.grey[500], fontSize: 16),
+            )),
+            Wrap(
+                children: List<Widget>.generate(3, (index) {
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.grey[500],
+                  child: CircleAvatar(
+                    radius: 25,
+                    backgroundImage: AssetImage("img/" + images[index]),
+                  ),
+                ),
+              );
+            }))
+          ],
+        ));
+  }
+}
